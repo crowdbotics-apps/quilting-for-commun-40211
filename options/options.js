9b1318c4aef8754409f6a65a7f4296c16ae6c949
Crowdbotics <team@crowdbotics.com>
2023-04-25 20:24:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quilting-for-commun-40211",
+  url: "https://quilting-for-commun-40211.botics.co",
+  api: "https://quilting-for-commun-40211.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
